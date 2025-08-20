7fa8cf28da11eec5ec3ea96f07457e40250f7dde
sss777-s <ssamie220077@gmail.com>
2025-08-20 20:52:17 +0100

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,8 @@
 @echo off
-REM Fake gradlew.bat for Windows builds
-echo Gradle wrapper not available
+REM Gradle wrapper batch file
+SETLOCAL
+if not defined JAVA_HOME (
+  echo JAVA_HOME not set. Please install Java 17+ and set JAVA_HOME.
+) 
+java -cp "%~dp0\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+ENDLOCAL
